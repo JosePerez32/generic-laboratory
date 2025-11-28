28a7358a93e289e0fbb9e9203e4293fa39cd0aeb
jcperez <jose.perez@infocura.be>
2025-11-28 23:20:52 +0100

    Url api edited in the frontend.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'http://https://idk-five-liard.vercel.app',
         changeOrigin: true,
         secure: false
       }
